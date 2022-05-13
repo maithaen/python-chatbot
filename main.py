8609f6e1a2e4483a16643508844cede9ed4ce430
Maithaen <maithaen122@gmail.com>
2022-05-13 21:26:00 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Create chatbot
+
+from nltk.chat.util import Chat, reflections
+#['(.*)ຄຳຖາມ(.*)', ['ຄຳຕອບ1', 'ຄຳຕອບ2', 'ຄຳຕອບອື່ນໆ']] \\(.*) ສ່ວນຕໍ່ທ້າຍ
+pairs = [
+['ສະບາຍດີ(.*)', ['ເຈົ້າ', 'ໂດຍ', 'ມີຫຍັງໃຫ້ຊ່ວຍບໍ່']],
+['ມີຫຍັງໃຫ້ຊ່ວຍ(.*)', ['ເຈົ້າ', 'ໂດຍ', 'ມີຫຍັງໃຫ້ຊ່ວຍບໍ່']],
+['ເຮັດຫຫຍັງຢູ່(.*)', ['ນອນຫຼີ້ນ']]
+]
+chat = Chat(pairs, reflections)
+#print(reflections)
+
+#chat.converse()
+chat.converse()
